11235670dc0731319f5c8365207a821bcbbd10c4
L-cir94 <luca.cirigliano94@hotmail.it>
2023-06-15 08:26:25 +0200

    axios

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,40 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+
+
 
 export default {
     components: {
-        HelloWorld,
+
+    },
+    data() {
+        return {
+            baseAPI: 'http://127.0.0.1:8000/',
+            projectPath: 'api/projects',
+            loading: true,
+            projects: null,
+            error: null
+        }
+    },
+    methods: {
+        getProjects(url) {
+            axios
+                .get(url)
+                .then(response => {
+                    console.log(response)
+                    this.projects = response.data.projects
+                    this.loading = false
+                })
+                .catch(error => {
+                    console.log(error);
+                    this.error = error.message
+                })
+        }
+    },
+    mounted() {
+        const url = this.baseAPI + this.projectPath
+        this.getProjects(url)
+        /*   console.log(url) */
     }
 }
 </script>
@@ -11,17 +42,16 @@ export default {
 <template>
     <section class="vue-home">
         <div class="container">
-            <div class="row">
-                <div class="col-12 d-flex flex-column justify-content-center align-items-center vh-100">
-                    <div class="logos">
-                        <a href="https://vitejs.dev" target="_blank">
-                            <img src="/vite.svg" class="logo" alt="Vite logo" />
-                        </a>
-                        <a href="https://vuejs.org/" target="_blank">
-                            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-                        </a>
+            <h1>I miei progetti</h1>
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 g-3">
+                <div class="col" v-for="project in projects">
+                    <div class="card h-100">
+                        <img class="card-img-top" src="holder.js/100x180/" alt="Title">
+                        <div class="card-body">
+                            <h4 class="card-title">{{ project.title }}</h4>
+                            <p class="card-text">{{ project.content }}</p>
+                        </div>
                     </div>
-                    <HelloWorld />
                 </div>
             </div>
         </div>
@@ -30,31 +60,4 @@ export default {
 
 <style lang="scss">
 @use './styles/general.scss';
-
-.vue-home {
-    color: #2c3e50;
-    background: #181818;
-    transition: color 0.5s, background-color 0.5s;
-    line-height: 1.6;
-    font-family: Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
-        Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
-    font-size: 15px;
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-}
-
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-}
-
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
