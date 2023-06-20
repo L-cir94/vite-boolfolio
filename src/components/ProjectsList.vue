c52424808913540d5edfccea02d93d19a4a44069
L-cir94 <luca.cirigliano94@hotmail.it>
2023-06-20 16:45:21 +0200

    commit for check tutor

--- src/components/ProjectsList.vue
+++ src/components/ProjectsList.vue
@@ -1,5 +1,6 @@
 <script>
 import axios from 'axios'
+
 export default {
     name: "ProjectsList",
     data() {
@@ -91,8 +92,8 @@ export default {
                                 <span class="badge mx-1" v-for="technology in project.technologies"> {{
                                     technology.name }}</span>
                             </div>
-                            <div class="types">
-                                <span class="badge bg-danger p-2 mx-1" v-for="type in project">{{ type.name }}</span>
+                            <div class="types" v-if="type">
+                                <span class="badge bg-danger p-2 mx-1" v-for="type in project.type">{{ type.name }}</span>
                             </div>
                         </div>
                         <div class="card-footer text-center">
