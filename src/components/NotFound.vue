4ee2a677144979a23a16642a056ec67f22eda7cf
L-cir94 <luca.cirigliano94@hotmail.it>
2023-06-21 16:07:05 +0200

    404 not found

---
+++ src/components/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="row mt-5 " >
+            <h1>Page not found</h1>
+            <route-link :to="{ name: 'home' }">
+                Torna alla HomePage
+            </route-link>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "Notfound",
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/router.js
+++ src/router.js
@@ -3,32 +3,34 @@ import AboutView from './views/AboutView.vue'
 import ContactsView from './views/ContactsView.vue'
 import ProjectsView from './views/ProjectsView.vue'
 import SingleProject from './components/SingleProject.vue'
+import NotFound from './components/NotFound.vue'
 
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
 
-    {
-        'path': '/about',
-        'name': 'about',
-        'component': AboutView
-    },
-    {
-        'path': '/contacts',
-        'name': 'contacts',
-        'component': ContactsView
-    },
-    {
-        'path': '/projects',
-        'name': 'projects',
-        'component': ProjectsView
-    },
-    {
-        'path':'/project/:slug',
-        'name': 'single-project',
-        'component': SingleProject
-    }
+        {
+            path: '/about',
+            name: 'about',
+            component: AboutView
+        },
+        {
+            path: '/contacts',
+            name: 'contacts',
+            component: ContactsView
+        },
+        {
+            path: '/projects',
+            name: 'projects',
+            component: ProjectsView
+        },
+        {
+            path: '/project/:slug',
+            name: 'single-project',
+            component: SingleProject
+        },
+        { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
 
     ]
 })
